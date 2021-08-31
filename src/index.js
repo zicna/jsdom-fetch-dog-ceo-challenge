d4936c8bf3f891f2bb7aac90627eecd77bbc7e64
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-08-30 23:26:53 -0500

    commit tree 8add193b013ede01556e520e28b2d81cbad01dd4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,73 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+//***************************************************************************** */
+//* Challenge 1
+
+document.addEventListener("DOMContentLoaded", () => {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  const dogImageCntainer = document.getElementById("dog-image-container");
+  const breedUrl = "https://dog.ceo/api/breeds/list/all";
+  const dogBreeds = document.getElementById("dog-breeds");
+  const breedDropdown = document.getElementById("breed-dropdown");
+
+  let imgs = fetch(imgUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then((json) => {
+      itterateOverImages(json["message"]);
+    });
+
+  function itterateOverImages(json) {
+    for (msg of json) {
+      createImage(msg);
+    }
+  }
+
+  function createImage(msg) {
+    let imgTag = document.createElement("img");
+    imgTag.src = msg;
+    dogImageCntainer.appendChild(imgTag);
+  }
+  //***************************************************************************** */
+  //* Challenge 2
+
+  let breeds = fetch(breedUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      itterateOverBreed(json["message"]);
+    });
+  function itterateOverBreed(msg) {
+    for (breed in msg) {
+      // if()
+      let li = document.createElement("li");
+      li.innerHTML = breed;
+      li.addEventListener("click", () => {
+        li.style.color = "red";
+      });
+      dogBreeds.appendChild(li);
+    }
+  }
+  //   console.log( `${breedDropdown.options[breedDropdown.selectedIndex].value == false}`)
+  breedDropdown.addEventListener("change", () => {
+    let value = breedDropdown.options[breedDropdown.selectedIndex].value;
+    if (value) {
+      dogBreeds.innerHTML = "";
+    }
+  });
+});
+
+//***************************************************************************** */
+
+//***************************************************************************** */
+
+//***************************************************************************** */
+
+// fetch('http://api.open-notify.org/astros.json')
+// .then(function(response) {
+//   return response.json();
+// }).then(function(json) {
+//   console.log(json);
+//   console.log(`Holy cow! There are ${json["number"]} humans in space.`);
+// });
